1547bd87a865b98807c34bc46c20241289cc1003
Michael <michael@wilkowski.net>
2023-10-22 15:44:23 -0400

    _

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description="Display Pokedex entries from a specific generation on an e-paper display.")
     parser.add_argument('--generation', type=str, default='', help="Name of the Pokemon generation to display (e.g., 'Gen1', 'Gen2', etc.).")
     parser.add_argument('--pokedex', type=int, default=0, help="Number of the Pokemon's Pokedex entry (e.g., 1, 2, etc.).")
-    parser.add_argument('--slideshow', action='store_false', help="Start a slideshow to display the Pokemon entries")
+    parser.add_argument('--slideshow', action='store_true', help="Start a slideshow to display the Pokemon entries")
     parser.add_argument('--sorted', action='store_true', help="Order to display the Pokemon entries")
 
     args = parser.parse_args()
