2a577ca94d23155194c5929e04b4cc52b9a85db7
Michael <michael@wilkowski.net>
2023-10-22 14:46:55 -0400

    _

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from lib import pokedex
 def main():
     parser = argparse.ArgumentParser(description="Display Pokedex entries from a specific generation on an e-paper display.")
     parser.add_argument('--generation', type=str, default='', help="Name of the Pokemon generation to display (e.g., 'Gen1', 'Gen2', etc.).")
+    parser.add_argument('--pokedex', type=int, default=0, help="Number of the Pokemon's Pokedex entry (e.g., 1, 2, etc.).")
 
     args = parser.parse_args()
 
-    pokedex.display(args.generation)
+    pokedex.display(args.generation, args.pokedex)
 
 if __name__ == "__main__":
     main()
