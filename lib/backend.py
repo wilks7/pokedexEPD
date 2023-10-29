ca26d0c21c8c79b8fc4e4d047b2f39a04f0401d6
Michael <michael@wilkowski.net>
2023-10-29 13:54:34 -0400

    _

--- lib/backend.py
+++ lib/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from display import display_pokemon, InvalidGenerationError
 
 app = Flask(__name__)
@@ -10,3 +10,17 @@ def get_pokedex_entry(pokemon_number, generation, version):
         return jsonify(success=True, message="Pokedex entry displayed successfully!")
     except InvalidGenerationError as e:
         return jsonify(success=False, message=str(e)), 400
+
+
+@app.route('/pokedex', methods=['GET', 'POST'])
+def pokedex_form():
+    if request.method == 'POST':
+        pokemon_number = request.form.get('pokemon_number')
+        generation = request.form.get('generation')
+        version = request.form.get('version')
+        
+        # Redirect to the URL structure you specified earlier to display the chosen Pokémon
+        return redirect(url_for('get_pokedex_entry', pokemon_number=pokemon_number, generation=generation, version=version))
+    
+    # If method is GET, serve the HTML form
+    return render_template('pokedex_form.html')
