58115f2614dd4c1895274f6d2a1264d23a13c1f9
Michael <michael@wilkowski.net>
2023-10-29 14:06:05 -0400

    _

--- lib/pokedex_display.py
+++ lib/pokedex_display.py
@@ -1,4 +1,4 @@
-from lib.constants import POKEMON_RANGES
+from lib.constants import GENERATIONS
 from lib.pokedex_omni_epd import PokedexEPD
 from time import sleep
 import random
@@ -9,15 +9,22 @@ class InvalidGenerationError(Exception):
 
 def display_pokemon(pokedex, generation, version=None):
     _check_generation(pokedex, generation)
+    if version:  # If a version is provided, check its validity
+        _check_version_for_generation(generation, version)
     
     pokedexEPD = PokedexEPD(generation, version)
     pokedexEPD.display(pokedex)
+
 
 def start_slideshow(generation, version=None):
     sorted_entries = bool(get_config("Slideshow", "sorted")) or True
     delay = int(get_config("Slideshow", "delay")) or 10
 
-    start, end = POKEMON_RANGES.get(generation, None)
+    gen_data = GENERATIONS.get(generation, None)
+    if not gen_data:
+        raise InvalidGenerationError(f"Invalid Generation {generation}.")
+        
+    start, end = gen_data['range']
     pokedex_entries = list(range(start, end + 1))
     
     if not sorted_entries:
@@ -28,9 +35,19 @@ def start_slideshow(generation, version=None):
         sleep(delay)
 
 def _check_generation(pokedex, generation):
-    if 0 < generation < 10:
-        (start, end) = POKEMON_RANGES[generation]
-        if pokedex > end:
+    gen_data = GENERATIONS.get(generation, None)
+    if gen_data:
+        start, end = gen_data['range']
+        if not (start <= pokedex <= end):
             raise InvalidGenerationError(f"Pokedex number {pokedex} does not belong to Generation {generation}.")
     else:
         raise InvalidGenerationError(f"Invalid Generation {generation}.")
+    
+def _check_version_for_generation(generation, version):
+    gen_data = GENERATIONS.get(generation, None)
+    if not gen_data:
+        raise InvalidGenerationError(f"Invalid Generation {generation}.")
+
+    if version not in gen_data['versions']:
+        raise InvalidGenerationError(f"Version {version} is not valid for Generation {generation}.")
+
