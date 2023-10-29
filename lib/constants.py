14a82d0cdfd952de4cfe1602a7ef349fdd5d7d28
Michael <michael@wilkowski.net>
2023-10-29 14:13:48 -0400

    _

--- lib/constants.py
+++ lib/constants.py
@@ -1,10 +1,43 @@
-POKEMON_RANGES = {
-    1: (1, 151),
-    2: (152, 251),
-    3: (252, 386),
-    4: (387, 493),
-    5: (494, 649),
-    6: (650, 721),
-    7: (722, 809),
-    8: (810, 898)  # Assuming Gen 8 goes up to #898, adjust as needed
+# Define Generations, Pokedex Ranges, and Versions
+GENERATIONS = {
+    1: {
+        "title": "Generation-I",
+        "range": (1, 151),
+        "versions": ["red-blue", "yellow"]
+    },
+    2: {
+        "title": "Generation-II",
+        "range": (152, 251),
+        "versions": ["gold-silver", "crystal"]
+    },
+    3: {
+        "title": "Generation-III",
+        "range": (252, 386),
+        "versions": ["ruby-sapphire", "emerald", "fire-red-leaf-green"]
+    },
+    4: {
+        "title": "Generation-IV",
+        "range": (387, 493),
+        "versions": ["diamond-pearl", "platinum", "heart-gold-soul-silver"]
+    },
+    5: {
+        "title": "Generation-V",
+        "range": (494, 649),
+        "versions": ["black-white", "black-2-white-2"]
+    },
+    6: {
+        "title": "Generation-VI",
+        "range": (650, 721),
+        "versions": ["x-y", "omega-ruby-alpha-sapphire"]
+    },
+    7: {
+        "title": "Generation-VII",
+        "range": (722, 809),
+        "versions": ["sun-moon", "ultra-sun-ultra-moon", "let's-go-pikachu-let's-go-eevee"]
+    },
+    8: {
+        "title": "Generation-VIII",
+        "range": (810, 898),  # This might change with new Pokémon releases
+        "versions": ["sword-shield"]
+    }
 }
